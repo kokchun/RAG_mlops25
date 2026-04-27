427ed4acec64e8fa317a842c50f36395acc2d31b
kokchun <kokchungiang@gmail.com>
2026-04-27 10:30:37 +0200

    setup vector db

--- rag/src/rag/setup/ingestion.py
+++ rag/src/rag/setup/ingestion.py
@@ -1,9 +1,13 @@
 from rag.backend.constants import DATA_PATH, VECTOR_DB_PATH
 import lancedb
 
+
 # TODO: create a knowledge_base with an appropriate table
 def setup_vector_db(path):
-    vector_db = lancedb.connect(uri = path)
+    vector_db = lancedb.connect(uri=path)
+    vector_db.create_table("articles", schema=Article, exist_ok=True)
+
+    return vector_db
 
 
 def ingest_docs_to_vector_db(table):
