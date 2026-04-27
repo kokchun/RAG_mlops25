e82ce6831fbbaf20b9536035b7416b092fa5031c
kokchun <kokchungiang@gmail.com>
2026-04-27 10:29:04 +0200

    setup vector db

---
+++ rag/src/rag/setup/ingestion.py
@@ -0,0 +1,19 @@
+from rag.backend.constants import DATA_PATH, VECTOR_DB_PATH
+import lancedb
+
+# TODO: create a knowledge_base with an appropriate table
+def setup_vector_db(path):
+    vector_db = lancedb.connect(uri = path)
+
+
+def ingest_docs_to_vector_db(table):
+    for file in DATA_PATH.glob("*.txt"):
+        with open(file) as f:
+            content = f.read()
+
+        # TODO: put the raw text or content into knowledge_base
+
+        # print(file.name)
+        # print(content)
+
+    # print(VECTOR_DB_PATH)
